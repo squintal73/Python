f8d22d37e8dfcc9bee3ab366dfd20ef2b6e519db
squintal73 <52165870+squintal73@users.noreply.github.com>
2020-07-02 23:54:14 -0300

    Update aula14.py

--- aula14.py
+++ aula14.py
@@ -1,6 +1,5 @@
-# DICTIONARY
-
-#chave/key - Valor/Value
+# AULA - DICTIONARY
+# CHAVE/key - VALOR/Value
 
 car={
     "Fabricante":"Honda",
@@ -9,18 +8,16 @@ car={
     "Cor":"Prata"
 }
 
-#car_get=car.get("Ano")
-
-#print(car["Fabricante"])
-
-#print(car_get)
-
-#for x in car:
-   # print(x) # CHAVES
-   # print(car[x]) # VALORES
-
 if "Modelo" in car:
     print(car["Modelo"])
 
 for c,v in car.items():
     print(c + " : " + v)
+
+#car_get=car.get("Ano")
+#print(car["Fabricante"])
+#print(car_get)
+#for x in car:
+   # print(x) # CHAVES
+   # print(car[x]) # VALORES
+
