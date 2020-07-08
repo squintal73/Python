cb1ec950ed5f364880c23b996245706f7e133f52
squintal73 <sidnei_quintal@yahoo.com.br>
2020-07-08 02:22:51 -0300

    V009

---
+++ API/consulta_cep_metodo_get.py
@@ -0,0 +1,31 @@
+# Consultar API viacep
+# Fazendo uma requisição GET
+# Data: 07/07/2020
+# Autor: Sidnei Quintal
+# V001.
+
+import requests
+import os
+
+os.system("clear")
+
+cep=input("Digite o CEP : ")
+
+def consulta_cep(cep):
+    url='http://viacep.com.br/ws/%s/json'%cep
+    r = requests.get(url)
+    r_json=r.json()
+    logradouro = r_json['logradouro']
+    localidade = r_json['localidade']
+    status=r.status_code
+    #print("Status de Conexao: " + str(status))
+    return  logradouro,localidade
+    
+if __name__ == '__main__':
+    logradouro, localidade = consulta_cep(cep)
+    print ("Localidade: " + localidade)
+    print ("Lougradouro: " + logradouro)
+    
+
+
+
