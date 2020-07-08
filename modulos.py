123da98adc688c19d4bea1a865bea5901671a596
Squintal <sidnei_quintal@yahoo.com.br>
2020-07-08 10:55:44 -0300

    VOO10 VOO10

---
+++ modulos.py
@@ -0,0 +1,12 @@
+# Modulos
+# DATA 08/07/2020
+# Autor: Sidnei Quintal
+# Documentos : https://docs.python.org/3/library/sys.html?highlight=sys#module-sys
+
+# import sys # import o modulo todo
+# print(sys.platform)
+from sys import platform as sidnei # import apenas (platform)pode dar um alias
+print(sidnei)
+
+
+
